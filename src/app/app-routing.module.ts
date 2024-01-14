729f11bb93395176be62f5d56e6fd9f7667afe99
Justin Loveless <justin.loveless@hp.com>
2024-01-13 19:16:23 -0600

    add nav menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { VideosComponent } from './pages/videos/videos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'videos',
+    component: VideosComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
